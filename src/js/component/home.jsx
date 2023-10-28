3dba34516fc93c020e68ebc7c44613b773144fc9
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-10-28 16:56:07 +0000

    navbar align, navbar sticky

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,13 +6,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light" >
+		<nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark text-light justify-content-between" >
   			<div className="container-fluid" >
     			<a className="navbar-brand" href="#">Start Bootstrap</a>
     				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
      					<span className="navbar-toggler-icon"></span>
    					 </button>
-    			<div className="collapse navbar-collapse" id="navbarNav">
+    			<div className="collapse navbar-collapse flex-grow-0 ml-auto" id="navbarNav">
 					<ul className="navbar-nav">
 						<li className="nav-item">
 							<a className="nav-link active" aria-current="page" href="#">Home</a>
