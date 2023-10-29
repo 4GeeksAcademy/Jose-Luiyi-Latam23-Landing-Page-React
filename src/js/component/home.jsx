71d69cd52db1ea39b3424a181d216688993de4cd
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-10-29 21:51:07 +0000

    NavBar toggle button component indented

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,15 @@ const NavBar = () => {
 		<nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark text-light justify-content-between" >
   			<div className="container-fluid" >
     			<a className="navbar-brand" href="#">Start Bootstrap</a>
-    				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    				<button 
+					className="navbar-toggler" 
+					type="button" 
+					data-bs-toggle="collapse" 
+					data-bs-target="#navbarNav" 
+					aria-controls="navbarNav" 
+					aria-expanded="false" 
+					aria-label="Toggle navigation"
+					>
      					<span className="navbar-toggler-icon"></span>
    					 </button>
     			<div className="collapse navbar-collapse flex-grow-0 ml-auto" id="navbarNav">
