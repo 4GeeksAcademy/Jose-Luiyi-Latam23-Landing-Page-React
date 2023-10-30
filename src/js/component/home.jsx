6761f21fc85b009b77dc0db047456759cb3f170c
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-10-30 00:17:48 +0000

    cards added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -41,7 +41,7 @@ const NavBar = (props) => {
 
 NavBar.propTypes = {
 companyName: PropTypes.string,
-navItems: PropTypes.string,
+navItems: PropTypes.array,
 }
 
 const Jumbotron = () => {
@@ -51,8 +51,42 @@ const Jumbotron = () => {
 };
 
 const Cards = () => {
+
 	return (
-		<div className="">Cards go here</div>
+		<div>
+			<div className="card">
+				<img src="..." className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+			<div className="card">
+				<img src="..." className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+			<div className="card">
+				<img src="..." className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+			<div className="card">
+				<img src="..." className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
 	);
 };
 
