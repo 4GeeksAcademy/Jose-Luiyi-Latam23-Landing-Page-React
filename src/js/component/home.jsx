e262be8d3b300eb0081050c1afd50438d5cc524c
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-10-31 13:14:33 +0000

    footer text-color to light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -114,7 +114,7 @@ const Footer = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-5">
       <div className="container-fluid">
-        <div className="row text-center justify-content-center w-100">
+        <div className="row text-center text-light justify-content-center w-100">
           <h5 className="text">Copy right © Your Website 2023"</h5>
         </div>
       </div>
