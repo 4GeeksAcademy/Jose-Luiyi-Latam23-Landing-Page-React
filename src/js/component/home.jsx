ef73d095ad84a0e6e646c542f22367a33534fedc
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-10-31 13:36:31 +0000

    footer mt to 0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -115,7 +115,7 @@ const Footer = () => {
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <div className="row text-center text-light justify-content-center w-100">
-          <h5 className="text">Copy right © Your Website 2023"</h5>
+          <h5 className="text">Copy right © Your Website 2023</h5>
         </div>
       </div>
     </nav>
